1fc88d01a631b55aeaa6b8efc2abb87df87c77bf
yayanyang <yayanyang@gmail.com>
2016-11-07 15:44:19 +0800

    update

---
+++ haohelper-iop-api/src/main/java/com/haohelper/iop/InternetOfPeopleService.java
@@ -0,0 +1,31 @@
+package com.haohelper.iop;
+
+import io.vertx.codegen.annotations.ProxyClose;
+import io.vertx.codegen.annotations.ProxyGen;
+import io.vertx.codegen.annotations.VertxGen;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+
+@ProxyGen
+@VertxGen
+interface InternetOfPeopleService {
+
+    /**
+     * Method to create the proxy
+     * @param vertx vertx instance
+     * @param address address
+     * @return  the service proxy
+     */
+    static InternetOfPeopleService createProxy(Vertx vertx, String address) {
+        return new InternetOfPeopleServiceVertxEBProxy(vertx, address);
+    }
+
+    // Actual service operations here...
+    void hello(final String message,
+               final Handler<AsyncResult<String>> resultHandler);
+
+
+    @ProxyClose
+    void close();
+}
---
+++ haohelper-iop-api/src/main/java/com/haohelper/iop/package-info.java
@@ -0,0 +1,4 @@
+@ModuleGen(groupPackage = "com.haohelper.iop", name = "haohelper-iop-module")
+package com.haohelper.iop;
+
+import io.vertx.codegen.annotations.ModuleGen;
